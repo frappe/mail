e72ed1c110224b44926b9259da2a643550f0aa04
s-aga-r <sagarsharma.s312@gmail.com>
2023-12-24 10:14:37 +0530

    fix(ux): don't allow to add or remove rows

--- mail/mail/doctype/fm_domain/fm_domain.js
+++ mail/mail/doctype/fm_domain/fm_domain.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("FM Domain", {
-// 	refresh(frm) {
+frappe.ui.form.on("FM Domain", {
+	refresh(frm) {
+        frm.trigger("set_domain_owner");
+        frm.trigger("make_records_table_read_only");
+	},
 
-// 	},
-// });
+    make_records_table_read_only(frm) {
+        ["sending_records", "receiving_records", "tracking_records"].forEach((field) => {
+            frm.set_df_property(field, "cannot_add_rows", true);
+            frm.set_df_property(field, "cannot_delete_rows", true);
+        });  
+    },
+
+    set_domain_owner(frm) {
+        if (frm.doc.__islocal && !frm.doc.domain_owner && frappe.session.user) {
+            frm.set_value("domain_owner", frappe.session.user);
+        }
+    }
+});
