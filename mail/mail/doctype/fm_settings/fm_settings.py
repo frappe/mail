64efb35d5c2394bce8b07b5ed7b852e530b482fe
s-aga-r <sagarsharma.s312@gmail.com>
2023-12-24 10:17:06 +0530

    feat: option to set SPF Host, Default DKIM Selector and Recommended TTL

--- mail/mail/doctype/fm_settings/fm_settings.py
+++ mail/mail/doctype/fm_settings/fm_settings.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class FMSettings(Document):
-	pass
+	def validate(self):
+		self.validate_default_dkim_bits()
+
+	def validate_default_dkim_bits(self):
+		if self.default_dkim_bits < 1024:
+			frappe.throw(_("DKIM Bits must be greater than 1024."))
