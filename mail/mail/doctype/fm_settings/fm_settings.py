b66ba92e89613d05a901ecb4aa00d66463f02a53
s-aga-r <sagarsharma.s312@gmail.com>
2024-01-04 21:37:32 +0530

    feat: Server DNS Records in FM Settings

--- mail/mail/doctype/fm_settings/fm_settings.py
+++ mail/mail/doctype/fm_settings/fm_settings.py
@@ -10,7 +10,39 @@ from frappe.model.document import Document
 class FMSettings(Document):
 	def validate(self):
 		self.validate_default_dkim_bits()
+		self.generate_dns_records()
 
 	def validate_default_dkim_bits(self):
 		if cint(self.default_dkim_bits) < 1024:
 			frappe.throw(_("DKIM Bits must be greater than 1024."))
+
+	def generate_dns_records(self):
+		self.dns_records.clear()
+
+		records = []
+		category = "Server Record"
+
+		# A Record
+		if self.public_ip:
+			records.append(
+				{
+					"category": category,
+					"type": "A",
+					"host": self.smtp_server,
+					"value": self.public_ip,
+					"ttl": self.default_ttl,
+				}
+			)
+
+		# TXT Record
+		records.append(
+			{
+				"category": category,
+				"type": "TXT",
+				"host": f"{self.spf_host}.{self.primary_domain_name}",
+				"value": f"v=spf1 a:{self.smtp_server} ~all",
+				"ttl": self.default_ttl,
+			}
+		)
+
+		self.extend("dns_records", records)
