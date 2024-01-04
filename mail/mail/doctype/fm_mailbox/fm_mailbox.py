a83045a41267a93b44eaa3dfc5663a85f3be93c2
s-aga-r <sagarsharma.s312@gmail.com>
2024-01-04 22:18:35 +0530

    chore: add field Sent and Error in FM Recipient

--- mail/mail/doctype/fm_mailbox/fm_mailbox.py
+++ mail/mail/doctype/fm_mailbox/fm_mailbox.py
@@ -7,22 +7,12 @@ from frappe.model.document import Document
 
 
 class FMMailbox(Document):
-	def validate(self):
-		self.validate_domain()
+	def validate(self) -> None:
 		self.validate_email()
+		self.validate_domain()
 		self.validate_display_name()
 
-	def validate_domain(self):
-		active, verified = frappe.db.get_value(
-			"FM Domain", self.domain_name, ["active", "verified"]
-		)
-
-		if not active:
-			frappe.throw(_("Domain {0} is inactive.").format(frappe.bold(self.domain_name)))
-		if not verified:
-			frappe.throw(_("Domain {0} is not verified.").format(frappe.bold(self.domain_name)))
-
-	def validate_email(self):
+	def validate_email(self) -> None:
 		email_domain = self.email.split("@")[1]
 
 		if email_domain != self.domain_name:
@@ -32,6 +22,16 @@ class FMMailbox(Document):
 				)
 			)
 
-	def validate_display_name(self):
+	def validate_domain(self) -> None:
+		active, verified = frappe.db.get_value(
+			"FM Domain", self.domain_name, ["active", "verified"]
+		)
+
+		if not active:
+			frappe.throw(_("Domain {0} is inactive.").format(frappe.bold(self.domain_name)))
+		if not verified:
+			frappe.throw(_("Domain {0} is not verified.").format(frappe.bold(self.domain_name)))
+
+	def validate_display_name(self) -> None:
 		if self.is_new() and not self.display_name:
-			self.display_name = frappe.get_cached_value("User", self.user, "full_name")
+			self.display_name = frappe.db.get_value("User", self.user, "full_name")
--- mail/mail/doctype/fm_queue/fm_queue.py
+++ mail/mail/doctype/fm_queue/fm_queue.py
@@ -1,9 +1,55 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import string
+import secrets
+import smtplib
+from datetime import datetime
+from email.mime.text import MIMEText
 from frappe.model.document import Document
+from email.mime.multipart import MIMEMultipart
 
 
 class FMQueue(Document):
-	pass
+	def autoname(self):
+		self.name = f"{0}.{1}".format(
+			datetime.now().strftime("%Y%m%d%H%M%S"),
+			"".join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(11)),
+		)
+
+	def add_recipients(self, recipients: list):
+		for recipient in recipients:
+			self.append("recipients", {"recipient": recipient})
+
+	def get_recipients(self):
+		return [d.recipient for d in self.recipients]
+
+	def get_smtp_details(self):
+		return frappe._dict(
+			{
+				"server": frappe.db.get_single_value("FM Settings", "smtp_server", cache=True),
+				"use_tls": frappe.db.get_single_value("FM Settings", "use_tls", cache=True),
+				"port": frappe.db.get_single_value("FM Settings", "smtp_port", cache=True),
+			}
+		)
+
+	def sendmail(self):
+		msg = MIMEMultipart("alternative")
+		msg["From"] = self.sender
+		msg["To"] = ", ".join(self.get_recipients())
+		msg["Subject"] = self.subject
+		msg["Message-ID"] = f"<{self.name}@mail.bunniesbakery.in>"
+
+		if self.body:
+			if isinstance(self.body, bytes):
+				self.body = self.body.decode()
+
+			msg.attach(MIMEText(self.body, "html"))
+
+		smtp_details = self.get_smtp_details()
+		with smtplib.SMTP(smtp_details.server, smtp_details.port) as server:
+			if smtp_details.use_tls:
+				server.starttls()
+
+			server.send_message(msg)
