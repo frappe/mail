b3288ce8997b7ee6228ea4f9daa8b97f12336d6d
s-aga-r <sagarsharma.s312@gmail.com>
2024-02-05 13:47:38 +0530

    feat: sendmail using mail-agent

--- mail/mail/doctype/fm_outgoing_email/fm_outgoing_email.py
+++ mail/mail/doctype/fm_outgoing_email/fm_outgoing_email.py
@@ -1,18 +1,29 @@
 # Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+import dkim
+import json
 import frappe
-from mail.send import sendmail
+import string
+import secrets
+from email import policy
+from datetime import datetime
+from email.parser import Parser
+from typing import TYPE_CHECKING
+from email.utils import formatdate
+from email.mime.text import MIMEText
 from mail.utils import get_outgoing_server
 from frappe.model.document import Document
+from email.mime.multipart import MIMEMultipart
+from frappe.utils.password import get_decrypted_password
+from mail.mail.doctype.fm_agent_job.fm_agent_job import create_agent_job
+
+if TYPE_CHECKING:
+	from mail.mail.doctype.fm_agent_job.fm_agent_job import FMAgentJob
 
 
 class FMOutgoingEmail(Document):
 	def autoname(self) -> None:
-		import string
-		import secrets
-		from datetime import datetime
-
 		self.name = "{0}.{1}".format(
 			datetime.now().strftime("%Y%m%d%H%M%S"),
 			"".join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(11)),
@@ -20,20 +31,53 @@ class FMOutgoingEmail(Document):
 
 	def validate(self) -> None:
 		self.validate_server()
+		self.set_original_message()
 
 	def after_insert(self) -> None:
-		self.enqueue_sendmail()
+		self.sendmail()
 
 	def validate_server(self) -> None:
 		if not self.server:
 			self.server = get_outgoing_server()
 
-	def add_recipients(self, recipients: list) -> None:
-		for recipient in recipients:
-			self.append("recipients", {"recipient": recipient})
+	def set_original_message(self) -> None:
+		self.original_message = self.get_signed_message()
 
-	def get_recipients(self) -> None:
-		return [d.recipient for d in self.recipients]
+	def get_signed_message(self) -> str:
+		display_name = frappe.get_cached_value("FM Mailbox", self.sender, "display_name")
+		dkim_selector = frappe.get_cached_value(
+			"FM Domain", self.domain_name, "dkim_selector"
+		)
+		dkim_private_key = get_decrypted_password(
+			"FM Domain", self.domain_name, "dkim_private_key"
+		)
+
+		message = MIMEMultipart("alternative")
+		message["From"] = (
+			"{0} <{1}>".format(display_name, self.sender) if display_name else self.sender
+		)
+		message["To"] = ", ".join(self.get_recipients())
+		message["Subject"] = self.subject
+		message["Date"] = formatdate()
+		message["Message-ID"] = "<{0}@{1}>".format(self.name, self.server)
+
+		if self.body:
+			message.attach(MIMEText(self.body, "html"))
+
+		headers = [b"To", b"From", b"Subject"]
+		signature = dkim.sign(
+			message=message.as_bytes(),
+			domain=self.domain_name.encode(),
+			selector=dkim_selector.encode(),
+			privkey=dkim_private_key.encode(),
+			include_headers=headers,
+		)
+		message["DKIM-Signature"] = signature[len("DKIM-Signature: ") :].decode()
+
+		return message.as_string()
+
+	def sendmail(self) -> None:
+		create_agent_job(self.server, "Send Mail", self.original_message)
 
 	def update_status(self, status: str, error_log: str = None) -> None:
 		if status == "Sent":
@@ -46,19 +90,21 @@ class FMOutgoingEmail(Document):
 		self.status = status
 		self.db_update()
 
-	def enqueue_sendmail(self) -> None:
-		frappe.enqueue_doc(
-			self.doctype,
-			self.name,
-			"sendmail",
-			timeout=600,
-			enqueue_after_commit=True,
-		)
+	def get_recipients(self) -> None:
+		return [d.recipient for d in self.recipients]
 
-	def sendmail(self) -> None:
-		try:
-			sendmail(self)
-			self.update_status("Sent")
-		except Exception:
-			error_log = frappe.log_error(title="FM Outgoing Email")
-			self.update_status(status="Failed", error_log=error_log.name)
+
+def update_delivery_status(agent_job: "FMAgentJob") -> None:
+	if agent_job.job_type != "Send Mail":
+		return
+
+	original_message = json.loads(agent_job.request_data).get("data")
+	parsed_message = Parser(policy=policy.default).parsestr(original_message)
+	outgoing_mail = parsed_message["Message-ID"].split("@")[0].replace("<", "")
+	status = "Sent" if agent_job.status == "Completed" else "Failed"
+
+	frappe.db.set_value(
+		"FM Outgoing Email",
+		outgoing_mail,
+		{"status": status, "error_log": agent_job.error_log},
+	)
