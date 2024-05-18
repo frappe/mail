2a22b1b7253f19ac3bcab266b37c3a581ca0235c
s-aga-r <sagarsharma.s312@gmail.com>
2024-05-18 15:34:19 +0530

    fix: validate duplicate mail contact

--- mail/mail/doctype/mail_contact/mail_contact.py
+++ mail/mail/doctype/mail_contact/mail_contact.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from typing import Optional
 from mail.utils import is_system_manager
 from frappe.model.document import Document
@@ -11,12 +12,21 @@ class MailContact(Document):
 	def before_validate(self) -> None:
 		self.set_user()
 
+	def validate(self) -> None:
+		self.validate_duplicate_contact()
+
 	def set_user(self) -> None:
 		"""Set user as current user if not set."""
 
 		user = frappe.session.user
 		if not self.user or not is_system_manager(user):
 			self.user = user
+
+	def validate_duplicate_contact(self) -> None:
+		if frappe.db.exists("Mail Contact", {"email": self.email, "user": self.user}):
+			frappe.throw(
+				_("Mail Contact with email {0} already exists.").format(frappe.bold(self.email))
+			)
 
 
 def has_permission(doc: "Document", ptype: str, user: str) -> bool:
