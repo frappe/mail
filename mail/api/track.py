9f1eae8785ac4d5048fbe3a69a41e9fe80135dd8
s-aga-r <sagarsharma.s312@gmail.com>
2024-05-26 12:29:56 +0530

    refactor: use `frappe.utils` to return tracking pixel as response

--- mail/api/track.py
+++ mail/api/track.py
@@ -1,27 +1,29 @@
 import frappe
 from frappe.query_builder import Case
-from frappe.utils.response import redirect
 
 
 @frappe.whitelist(methods=["GET"], allow_guest=True)
 def open(id: str) -> None:
 	"""Updates Outgoing Mail opened status."""
 
-	if id:
+	try:
 		now = frappe.utils.now()
 		OM = frappe.qb.DocType("Outgoing Mail")
-		first_opened_at_condition = (
-			Case().when(OM.first_opened_at.isnull(), now).else_(OM.first_opened_at)
-		)
 		(
 			frappe.qb.update(OM)
 			.set(OM.opened, 1)
-			.set(OM.first_opened_at, first_opened_at_condition)
+			.set(
+				OM.first_opened_at,
+				Case().when(OM.first_opened_at.isnull(), now).else_(OM.first_opened_at),
+			)
 			.set(OM.last_opened_at, now)
 			.set(OM.opened_count, OM.opened_count + 1)
 			.where((OM.docstatus == 1) & (OM.tracking_id == id))
 		).run()
+	except Exception:
+		frappe.log_error(
+			title="Error Updating Outgoing Mail Opened Status", message=frappe.get_traceback()
+		)
+	finally:
 		frappe.db.commit()
-
-	frappe.response.location = "/assets/mail/images/pixel.png"
-	return redirect()
+		frappe.response.update(frappe.utils.get_imaginary_pixel_response())
