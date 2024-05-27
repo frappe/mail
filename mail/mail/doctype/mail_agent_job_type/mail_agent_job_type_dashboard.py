306ec768b2562c54bdc8de2c3cdcd420392cbced
s-aga-r <sagarsharma.s312@gmail.com>
2024-05-27 14:00:15 +0530

    refactor: use DocType `Links` to create connections

--- mail/mail/doctype/mail_agent_job_type/mail_agent_job_type_dashboard.py
+++
@@ -1,8 +0,0 @@
-from frappe import _
-
-
-def get_data():
-	return {
-		"fieldname": "job_type",
-		"transactions": [{"items": ["Mail Agent Job"]}],
-	}
--- mail/mail/doctype/mail_domain/mail_domain_dashboard.py
+++
@@ -1,8 +0,0 @@
-from frappe import _
-
-
-def get_data():
-	return {
-		"fieldname": "domain_name",
-		"transactions": [{"items": ["Mailbox", "Mail Alias"]}],
-	}
