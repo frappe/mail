b70884f3a4f5544b1613396fb8cf0899d451a3cb
s-aga-r <sagarsharma.s312@gmail.com>
2024-05-29 06:26:13 +0530

    chore: rename report `Open Mail Tracker` to `Mail Tracker`

--- mail/mail/report/mail_tracker/mail_tracker.js
+++ mail/mail/report/mail_tracker/mail_tracker.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Open Mail Tracker"] = {
+frappe.query_reports["Mail Tracker"] = {
 	"filters": [
 		{
 			fieldname: "from_date",
