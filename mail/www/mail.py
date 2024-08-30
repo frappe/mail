4daf6ce6cefa462dcb5877ee14b3ef54fee48967
s-aga-r <sagarsharma.s312@gmail.com>
2024-08-30 20:11:40 +0530

    chore: linter

--- mail/www/mail.py
+++ mail/www/mail.py
@@ -1,9 +1,10 @@
 import frappe
 
+
 def get_context(context):
-    context.no_cache = 1
-    csrf_token = frappe.sessions.get_csrf_token()
-    frappe.db.commit()
-    context = frappe._dict()
-    context.boot.csrf_token = csrf_token
-    return context
+	context.no_cache = 1
+	csrf_token = frappe.sessions.get_csrf_token()
+	frappe.db.commit()
+	context = frappe._dict()
+	context.boot.csrf_token = csrf_token
+	return context
