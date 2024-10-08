6156f71b980bcc219ade1f06bb693d3305e17781
s-aga-r <sagarsharma.s312@gmail.com>
2024-10-08 23:31:36 +0530

    chore: give link to DNS Records in warning

--- mail/mail/doctype/mail_settings/mail_settings.js
+++ mail/mail/doctype/mail_settings/mail_settings.js
@@ -29,8 +29,9 @@ frappe.ui.form.on("Mail Settings", {
     add_comments(frm) {
         if (frm.doc.root_domain_name && (!frm.doc.dns_provider || !frm.doc.dns_provider_token)) {
             let root_domain_name = `<b>${frm.doc.root_domain_name}</b>`;
+            let dns_record_list_link = `<a href="/app/dns-record">${__("DNS Records")}</a>`;
             let msg = __(
-                "DNS provider or token not configured. Please manually add the required SPF and DKIM records to the DNS provider for the domain {0} to ensure proper email authentication.", [root_domain_name]
+                "DNS provider or token not configured. Please manually add the {0} to the DNS provider for the domain {1} to ensure proper email authentication.", [dns_record_list_link, root_domain_name]
             );
             frm.dashboard.add_comment(msg, "yellow", true);
         }
