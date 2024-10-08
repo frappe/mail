cfccd59c600efed88c5702a905a040eca6e201f4
s-aga-r <sagarsharma.s312@gmail.com>
2024-10-08 23:22:36 +0530

    chore: warning on `DNS Record` if not verified

--- mail/mail/doctype/dns_record/dns_record.js
+++ mail/mail/doctype/dns_record/dns_record.js
@@ -1,9 +1,11 @@
 // Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 frappe.ui.form.on("DNS Record", {
-	refresh(frm) {
+    refresh(frm) {
         frm.trigger("add_actions");
-	},
+        frm.trigger("add_comments");
+    },
+
     add_actions(frm) {
         if (!frm.doc.__islocal) {
             frm.add_custom_button(__("Verify DNS Record"), () => {
@@ -11,6 +13,17 @@ frappe.ui.form.on("DNS Record", {
             }, __("Actions"));
         }
     },
+
+    add_comments(frm) {
+        if (!frm.doc.is_verified) {
+            let bold_mail_settings = `<b>${__("Mail Settings")}</b>`;
+            let msg = __(
+                "It seems that the DNS provider or token is not configured in the {0}. Please manually add this DNS record to your provider for the root domain to ensure proper email authentication.", [bold_mail_settings]
+            );
+            frm.dashboard.add_comment(msg, "yellow", true);
+        }
+    },
+
     verify_dns_record(frm) {
         frappe.call({
             doc: frm.doc,
