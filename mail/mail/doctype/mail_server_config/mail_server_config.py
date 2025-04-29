a715d5c7a5985630749be0ee389e435c16169b41
s-aga-r <sagarsharma.s312@gmail.com>
2025-04-29 23:03:50 +0530

    refactor: rename `proxy_trusted_networks` -> `server_proxy_trusted_networks`

--- mail/mail/doctype/mail_server_config/mail_server_config.py
+++ mail/mail/doctype/mail_server_config/mail_server_config.py
@@ -346,7 +346,7 @@ def get_config_toml(server: str) -> str | None:
 		"certificate": get_tls_certificates(server.tls_certificates),
 		"server": {
 			"hostname": server.server,
-			"proxy": {"trusted-networks": split_lines_or_empty(cluster.proxy_trusted_networks)},
+			"proxy": {"trusted-networks": split_lines_or_empty(cluster.server_proxy_trusted_networks)},
 			"max-connections": server.server_max_connections,
 			"listener": get_listeners(server.listeners or cluster.listeners),
 			"socket": {
