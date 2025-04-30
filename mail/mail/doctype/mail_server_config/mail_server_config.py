f4783249137441fdd92529c9420df97dcd2cdcb0
s-aga-r <sagarsharma.s312@gmail.com>
2025-04-30 09:51:55 +0530

    refactor: rename `max_allowed_packet_bytes` -> `max_allowed_packet`

--- mail/mail/doctype/mail_server_config/mail_server_config.py
+++ mail/mail/doctype/mail_server_config/mail_server_config.py
@@ -263,7 +263,7 @@ def get_config_toml(server: str) -> str | None:
 				)
 
 				if store.type == "mySQL":
-					config["max-allowed-packet"] = store.max_allowed_packet_bytes
+					config["max-allowed-packet"] = store.max_allowed_packet
 					config["pool"]["min-connections"] = store.min_connections
 
 			case "S3-compatible":
