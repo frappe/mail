146330b7e01ea0cdb2bc2c1e649517b49e0c84ab
s-aga-r <sagarsharma.s312@gmail.com>
2025-04-30 11:16:00 +0530

    refactor: rename `transaction_timeout_seconds` -> `transaction_timeout`

--- mail/mail/doctype/mail_server_config/mail_server_config.py
+++ mail/mail/doctype/mail_server_config/mail_server_config.py
@@ -236,7 +236,7 @@ def get_config_toml(server: str) -> str | None:
 					{
 						"cluster-file": store.cluster_file,
 						"transaction": {
-							"timeout": format_value_or_zero(store.transaction_timeout_seconds, "s"),
+							"timeout": format_value_or_zero(store.transaction_timeout, "s"),
 							"retry-limit": store.transaction_retry_limit,
 							"max-retry-delay": format_value_or_zero(
 								store.transaction_max_retry_delay_seconds, "s"
