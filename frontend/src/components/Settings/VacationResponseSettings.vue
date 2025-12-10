1a0beb74ef7ac35393333cf57237da70033fcffd
krantheman <akash.akasht.tom@gmail.com>
2025-12-10 15:08:04 +0530

    refactor(Vacation Response): generic client

--- frontend/src/components/Settings/VacationResponseSettings.vue
+++ frontend/src/components/Settings/VacationResponseSettings.vue
@@ -1,25 +1,26 @@
 <template>
-	<template v-if="account.doc">
+	<template v-if="vacationResponse.doc">
 		<h1>{{ __('Vacation Response') }}</h1>
 		<Switch
-			v-model="vacationResponseEnabled"
+			v-model="vacationResponse.doc.enabled"
 			:label="__('Enabled')"
 			:description="__('Auto-reply to incoming mails while you’re away.')"
+			class="!p-0"
 		/>
 		<FormControl
-			v-model="account.doc.vacation_from_date"
+			v-model="vacationResponse.doc.from_date"
 			type="datetime-local"
 			:label="__('From Date')"
 			variant="outline"
 		/>
 		<FormControl
-			v-model="account.doc.vacation_to_date"
+			v-model="vacationResponse.doc.to_date"
 			type="datetime-local"
 			:label="__('To Date')"
 			variant="outline"
 		/>
 		<FormControl
-			v-model="account.doc.vacation_response_subject"
+			v-model="vacationResponse.doc.subject"
 			:label="__('Subject')"
 			placeholder="Out of Office"
 			variant="outline"
@@ -30,29 +31,37 @@
 				editor-class="prose-sm min-h-[8rem] border rounded-b-lg border-t-0 p-2 max-w-none border-outline-gray-2"
 				:placeholder="__('Type something...')"
 				:fixed-menu="buttons"
-				:content="account.doc.vacation_response_html_body"
-				@change="(val: string) => (account.doc.vacation_response_html_body = val)"
+				:content="vacationResponse.doc.html_body"
+				@change="(val: string) => (vacationResponse.doc.html_body = val)"
 			/>
 		</div>
 		<Button
 			:label="__('Save')"
 			variant="solid"
 			:disabled="
-				account.get.loading ||
-				JSON.stringify(account.doc) === JSON.stringify(account.originalDoc)
+				vacationResponse.get.loading ||
+				JSON.stringify(vacationResponse.doc) ===
+					JSON.stringify(vacationResponse.originalDoc)
 			"
-			:loading="account.setVacationResponse?.loading"
+			:loading="updateVacationResponse.loading"
 			class="min-h-7"
-			@click="() => account.setVacationResponse.submit()"
+			@click="() => updateVacationResponse.submit()"
 		/>
 	</template>
 </template>
 
 <script setup lang="ts">
-import { computed, inject } from 'vue'
-import { Button, FormControl, Switch, TextEditor, createDocumentResource } from 'frappe-ui'
+import { inject } from 'vue'
+import {
+	Button,
+	FormControl,
+	Switch,
+	TextEditor,
+	createDocumentResource,
+	createResource,
+} from 'frappe-ui'
 
-import { raiseToast } from '@/utils'
+import { convertHtmlToText, raiseToast } from '@/utils'
 import { useTextEditorButtons } from '@/utils/composables'
 
 import type { MailAccount } from '@/types/doctypes'
@@ -62,34 +71,31 @@ const dayjs = inject('$dayjs')
 
 const { buttons } = useTextEditorButtons()
 
-const account = createDocumentResource({
-	doctype: 'Mail Account',
+const vacationResponse = createDocumentResource({
+	doctype: 'Vacation Response',
 	name: user.data.name,
 	transform: (doc: MailAccount) => {
-		doc['vacation_from_date'] = dayjs(doc['vacation_from_date']).format('YYYY-MM-DDTHH:mm')
-		doc['vacation_to_date'] = dayjs(doc['vacation_to_date']).format('YYYY-MM-DDTHH:mm')
-	},
-	whitelistedMethods: {
-		setVacationResponse: {
-			method: 'set_vacation_response',
-			makeParams: () => ({
-				enabled: account.doc.vacation_response_enabled,
-				from_date: account.doc.vacation_from_date,
-				to_date: account.doc.vacation_to_date,
-				subject: account.doc.vacation_response_subject,
-				html_body: account.doc.vacation_response_html_body,
-			}),
-			onSuccess: () => {
-				account.reload()
-				raiseToast(__('Vacation response updated.'))
-			},
-			onError: (error) => raiseToast(error.messages[0], 'error'),
-		},
+		doc['enabled'] = !!doc['enabled']
+		doc['from_date'] = dayjs(doc['from_date']).format('YYYY-MM-DDTHH:mm')
+		doc['to_date'] = dayjs(doc['to_date']).format('YYYY-MM-DDTHH:mm')
 	},
 })
 
-const vacationResponseEnabled = computed({
-	get: () => !!account.doc.vacation_response_enabled,
-	set: (val: boolean) => (account.doc.vacation_response_enabled = val ? 1 : 0),
+const updateVacationResponse = createResource({
+	url: 'mail.client.doctype.vacation_response.vacation_response.update_vacation_response',
+	makeParams: () => ({
+		user: user.data.name,
+		enabled: vacationResponse.doc.enabled,
+		from_date: vacationResponse.doc.from_date,
+		to_date: vacationResponse.doc.to_date,
+		subject: vacationResponse.doc.subject,
+		text_body: convertHtmlToText(vacationResponse.doc.html_body),
+		html_body: vacationResponse.doc.html_body,
+	}),
+	onSuccess: () => {
+		vacationResponse.reload()
+		raiseToast(__('Vacation response updated.'))
+	},
+	onError: (error) => raiseToast(error.messages[0], 'error'),
 })
 </script>
