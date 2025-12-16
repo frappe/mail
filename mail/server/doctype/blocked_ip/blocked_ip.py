a88f246709cee10822a13ce1350635ebabab29a7
s-aga-r <sagarsharma.s312@gmail.com>
2025-12-16 08:23:56 +0530

    refactor: `Blocked IP`

--- mail/server/doctype/blocked_ip/blocked_ip.py
+++ mail/server/doctype/blocked_ip/blocked_ip.py
@@ -9,7 +9,7 @@ from frappe.model.document import Document
 from frappe.utils import today
 
 from mail.backend import get_mail_backend_api
-from mail.utils import extract_filter_values, rename_keys
+from mail.utils import extract_filter_values
 from mail.utils.dns import get_host_by_ip
 
 
@@ -25,17 +25,19 @@ class BlockedIP(Document):
 		self.name = f"{self.cluster}|{self.ip_address}"
 
 	def db_insert(self, *args, **kwargs) -> None:
-		add_blocked_ip(self.cluster, self.ip_address)
+		BlockedIP._create(self.cluster, self.ip_address)
 
 	def load_from_db(self) -> "BlockedIP":
-		blocked_ip = fetch_blocked_ip_details(self.name)
+		cluster, ip_address = self.name.split("|")
+		blocked_ip = BlockedIP._read(cluster, ip_address)
 		return super(Document, self).__init__(blocked_ip)
 
 	def db_update(self) -> None:
 		raise NotImplementedError
 
 	def delete(self) -> None:
-		remove_blocked_ip(self.name)
+		cluster, ip_address = self.name.split("|")
+		BlockedIP._delete(cluster, ip_address)
 
 		if not frappe.flags.in_bulk_delete:
 			frappe.msgprint(_("Blocked IP removed successfully."), alert=True)
@@ -46,7 +48,7 @@ class BlockedIP(Document):
 		cluster, ip_address = extract_filter_values(filters, [{"cluster": "="}, {"ip_address": "like"}])
 
 		if cluster:
-			blocked_ips = fetch_blocked_ips(cluster, limit=page_length, text=ip_address)
+			blocked_ips = BlockedIP._read_all(cluster, limit=page_length, text=ip_address)
 			if not blocked_ips:
 				frappe.msgprint(_("No blocked IPs found."), alert=True)
 
@@ -66,96 +68,84 @@ class BlockedIP(Document):
 	def get_stats(**kwargs) -> dict:
 		return {}
 
+	@staticmethod
+	def _create(cluster: str, ip_address: str | list[str]) -> None:
+		ip_addresses = [ip_address] if isinstance(ip_address, str) else ip_address
+		request_data = []
+		for ip in ip_addresses:
+			request_data.append(
+				{
+					"type": "insert",
+					"prefix": None,
+					"values": [[f"server.blocked-ip.{ip}", ""]],
+					"assert_empty": True,
+				}
+			)
+
+		backend_api = get_mail_backend_api("Mail Cluster", cluster)
+		backend_api.request(
+			method="POST",
+			endpoint="/api/settings",
+			data=json.dumps(request_data),
+		)
+
+	@staticmethod
+	def _read(cluster: str, ip_address: str) -> None:
+		backend_api = get_mail_backend_api("Mail Cluster", cluster)
+		response = backend_api.request(
+			method="GET",
+			endpoint="api/settings/group",
+			params={"prefix": "server.blocked-ip", "limit": 1, "filter": ip_address},
+		)
+
+		blocked_ip = response.json()["data"]["items"][0]
+		return BlockedIP._format(blocked_ip, cluster)
+
+	@staticmethod
+	def _read_all(cluster: str, page: int = 1, limit: int = 10, text: str | None = None) -> list:
+		backend_api = get_mail_backend_api("Mail Cluster", cluster)
+		response = backend_api.request(
+			method="GET",
+			endpoint="api/settings/group",
+			params={"page": page, "prefix": "server.blocked-ip", "limit": limit, "filter": text},
+		)
+
+		data = response.json()["data"]
+		frappe.cache.set_value(get_total_cache_key(cluster, text), data["total"], expires_in_sec=600)
+
+		return [BlockedIP._format(item, cluster) for item in data["items"]]
+
+	@staticmethod
+	def _update() -> None:
+		raise NotImplementedError
+
+	@staticmethod
+	def _delete(cluster: str, ip_address: str | list[str]) -> None:
+		ip_addresses = [ip_address] if isinstance(ip_address, str) else ip_address
+		request_data = []
+		for ip in ip_addresses:
+			request_data.append({"type": "delete", "keys": [f"server.blocked-ip.{ip}"]})
+
+		backend_api = get_mail_backend_api("Mail Cluster", cluster)
+		backend_api.request(
+			method="POST",
+			endpoint="/api/settings",
+			data=json.dumps(request_data),
+		)
+
+	@staticmethod
+	def _format(blocked_ip: dict, cluster: str) -> dict:
+		return {
+			"cluster": cluster,
+			"ip_address": blocked_ip["_id"],
+			"name": f"{cluster}|{blocked_ip['_id']}",
+			"creation": today(),
+			"modified": today(),
+		}
+
 
 def get_total_cache_key(cluster_name: str, text: str | None = None) -> str:
 	"""Returns a cache key for total blocked IP count."""
 
 	text = text or ""
 	return f"{cluster_name}:blocked-ip:{text}:total"
-
-
-def add_blocked_ip(cluster_name: str, ip_address: str | list) -> None:
-	"""Adds a blocked ip to the mail server."""
-
-	if isinstance(ip_address, str):
-		ip_address = [ip_address]
-
-	request_data = []
-	for ip in ip_address:
-		request_data.append(
-			{
-				"type": "insert",
-				"prefix": None,
-				"values": [[f"server.blocked-ip.{ip}", ""]],
-				"assert_empty": True,
-			}
-		)
-
-	backend_api = get_mail_backend_api("Mail Cluster", cluster_name)
-	backend_api.request(
-		method="POST",
-		endpoint="/api/settings",
-		data=json.dumps(request_data),
-	)
-
-
-def fetch_blocked_ips(cluster_name: str, page: int = 1, limit: int = 10, text: str | None = None) -> list:
-	"""Fetches a list of blocked ips from the mail server."""
-
-	backend_api = get_mail_backend_api("Mail Cluster", cluster_name)
-	response = backend_api.request(
-		method="GET",
-		endpoint="api/settings/group",
-		params={"page": page, "prefix": "server.blocked-ip", "limit": limit, "filter": text},
-	)
-
-	data = response.json()["data"]
-	frappe.cache.set_value(get_total_cache_key(cluster_name, text), data["total"], expires_in_sec=600)
-
-	return [format_blocked_ip(item, cluster_name) for item in data["items"]]
-
-
-def fetch_blocked_ip_details(name: str) -> dict:
-	"""Fetches details of a specific blocked ip from the mail server."""
-
-	cluster_name, ip_address = name.split("|")
-	backend_api = get_mail_backend_api("Mail Cluster", cluster_name)
-	response = backend_api.request(
-		method="GET",
-		endpoint="api/settings/group",
-		params={"prefix": "server.blocked-ip", "limit": 1, "filter": ip_address},
-	)
-
-	blocked_ip = response.json()["data"]["items"][0]
-	return format_blocked_ip(blocked_ip, cluster_name)
-
-
-def remove_blocked_ip(name: str | list) -> None:
-	"""Removes a blocked ip from the mail server."""
-
-	if isinstance(name, str):
-		name = [name]
-
-	request_data = []
-	for n in name:
-		cluster_name, ip_address = n.split("|")
-		request_data.append({"type": "delete", "keys": [f"server.blocked-ip.{ip_address}"]})
-
-	backend_api = get_mail_backend_api("Mail Cluster", cluster_name)
-	backend_api.request(
-		method="POST",
-		endpoint="/api/settings",
-		data=json.dumps(request_data),
-	)
-
-
-def format_blocked_ip(blocked_ip: dict, cluster_name: str) -> dict:
-	"""Formats a blocked ip dictionary to match expected output."""
-
-	return {
-		"cluster": cluster_name,
-		"ip_address": blocked_ip["_id"],
-		"name": f"{cluster_name}|{blocked_ip['_id']}",
-		"creation": today(),
-		"modified": today(),
-	}
