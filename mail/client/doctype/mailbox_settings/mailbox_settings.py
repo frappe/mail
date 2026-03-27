52afe998df7949a0e16c7c7bc4b6eb171318f68d
s-aga-r <sagarsharma.s312@gmail.com>
2026-03-27 12:42:47 +0530

    chore: add permissions for `Mail User`

--- mail/client/doctype/mailbox_settings/mailbox_settings.py
+++ mail/client/doctype/mailbox_settings/mailbox_settings.py
@@ -6,6 +6,8 @@ from uuid import uuid7
 import frappe
 from frappe.model.document import Document
 
+from mail.utils.user import has_role, is_system_manager, is_tenant_admin
+
 
 class MailboxSettings(Document):
 	def autoname(self) -> None:
@@ -14,3 +16,29 @@ class MailboxSettings(Document):
 
 def on_doctype_update() -> None:
 	frappe.db.add_unique("Mailbox Settings", ["user", "mailbox_id"], constraint_name="unique_user_mailbox")
+
+
+def get_permission_query_condition(user: str | None = None) -> str:
+	user = user or frappe.session.user
+
+	if is_system_manager(user):
+		return ""
+
+	if has_role(user, "Mail User"):
+		return f"(`tabMailbox Settings`.user = '{user}')"
+
+	return "1=0"
+
+
+def has_permission(doc: Document, ptype: str, user: str | None = None) -> bool:
+	if doc.doctype != "Mailbox Settings":
+		return False
+
+	user = user or frappe.session.user
+
+	if is_system_manager(user):
+		return True
+	elif has_role(user, "Mail User"):
+		return doc.user == user
+
+	return False
