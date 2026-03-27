537671e33691a1287c5bd50cc6db503f4d5811dc
s-aga-r <sagarsharma.s312@gmail.com>
2026-03-27 12:37:55 +0530

    chore: set `uuid7` autonaming

--- mail/client/doctype/mailbox_settings/mailbox_settings.py
+++ mail/client/doctype/mailbox_settings/mailbox_settings.py
@@ -1,12 +1,15 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+from uuid import uuid7
+
 import frappe
 from frappe.model.document import Document
 
 
 class MailboxSettings(Document):
-	pass
+	def autoname(self) -> None:
+		self.name = str(uuid7())
 
 
 def on_doctype_update() -> None:
