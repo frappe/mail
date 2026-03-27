2727b8aa1651fb5c3fc5b3a643107566564a00d5
s-aga-r <sagarsharma.s312@gmail.com>
2026-03-27 12:35:11 +0530

    chore: add composite unique index on `user` and `mailbox_id`

--- mail/client/doctype/mailbox_settings/mailbox_settings.py
+++ mail/client/doctype/mailbox_settings/mailbox_settings.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MailboxSettings(Document):
 	pass
+
+
+def on_doctype_update() -> None:
+	frappe.db.add_unique("Mailbox Settings", ["user", "mailbox_id"], constraint_name="unique_user_mailbox")
