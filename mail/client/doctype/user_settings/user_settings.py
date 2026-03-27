7782db9e392863f596884e60bd8eeee6fb4625c4
s-aga-r <sagarsharma.s312@gmail.com>
2026-03-27 22:04:36 +0530

    fix: validate user's JMAP settings

--- mail/client/doctype/user_settings/user_settings.py
+++ mail/client/doctype/user_settings/user_settings.py
@@ -2,12 +2,57 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
-from mail.utils.user import has_role, is_system_manager
+from mail.jmap.connection import JMAPConnection, JMAPConnectionInfo
+from mail.jmap.services.mail.identity import IdentityService
+from mail.utils.user import get_tenant_for_user, has_role, is_system_manager, is_tenant_bound_user
 
 
 class UserSettings(Document):
+	def validate(self) -> None:
+		self.validate_jmap_settings()
+		self.validate_tenant_bound_user()
+
+	def validate_jmap_settings(self) -> None:
+		"""Validate the JMAP settings by connecting to the JMAP server and verifying the default outgoing email."""
+
+		info = JMAPConnectionInfo(self.server_url, self.username, self.get_password("app_password"))
+		connection = JMAPConnection(info)
+
+		if self.default_outgoing_email:
+			identity_service = IdentityService(self.user, connection)
+
+			if not identity_service.get_identity_id_by_email(self.default_outgoing_email):
+				frappe.throw(
+					_(
+						"Default Outgoing Email {0} is not found in the identities of the JMAP account."
+					).format(frappe.bold(self.default_outgoing_email))
+				)
+
+	def validate_tenant_bound_user(self) -> None:
+		"""Validate that if the user is tenant-bound, then the JMAP username must be the same as the User name and a Mail Principal Binding must exist for the user."""
+
+		if not is_tenant_bound_user(self.user):
+			return
+
+		if self.username != self.user:
+			frappe.throw(_("JMAP Username must be the same as the User name."))
+
+		tenant = get_tenant_for_user(self.user)
+
+		if not frappe.db.exists(
+			"Mail Principal Binding",
+			{"tenant": tenant, "principal_name": self.username},
+			"principal_name",
+		):
+			frappe.throw(
+				_("Account {0} is not bound to Tenant {1}").format(
+					frappe.bold(self.username), frappe.bold(tenant)
+				)
+			)
+
 	def _db_set(
 		self,
 		update_modified: bool = True,
