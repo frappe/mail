e0c72c323978aba2adf973ecb5fdd7c4c250bc5c
s-aga-r <sagarsharma.s312@gmail.com>
2026-04-21 12:08:15 +0530

    feat: `Blocked Email Address`

---
+++ mail/client/doctype/blocked_email_address/blocked_email_address.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class BlockedEmailAddress(Document):
+	pass
+
+
+def get_blocked_email_addresses(user: str) -> list[str]:
+	"""Returns a list of blocked email addresses for the given user."""
+
+	return frappe.db.get_all("Blocked Email Address", filters={"user": user}, plucks="email")
+
+
+def on_doctype_update() -> None:
+	frappe.db.add_unique(
+		"Blocked Email Address",
+		["user", "email"],
+		constraint_name="unique_user_blocked_email",
+	)
