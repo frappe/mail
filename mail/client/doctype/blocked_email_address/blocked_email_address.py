b4272bb6c9073aedd8c1d8c9876d35f0850c2d51
s-aga-r <sagarsharma.s312@gmail.com>
2026-04-22 21:51:38 +0530

    chore: update sieve on create and delete of blocked email

--- mail/client/doctype/blocked_email_address/blocked_email_address.py
+++ mail/client/doctype/blocked_email_address/blocked_email_address.py
@@ -6,7 +6,31 @@ from frappe.model.document import Document
 
 
 class BlockedEmailAddress(Document):
-	pass
+	def validate(self) -> None:
+		self.validate_duplicate_email()
+
+	def after_insert(self) -> None:
+		from mail.api.sieve import update_sieve_script_for_blocked_emails
+
+		update_sieve_script_for_blocked_emails(self.user)
+
+	def after_delete(self) -> None:
+		from mail.api.sieve import update_sieve_script_for_blocked_emails
+
+		update_sieve_script_for_blocked_emails(self.user)
+
+	def validate_duplicate_email(self) -> None:
+		"""Validates that the same email address is not blocked multiple times for the same user."""
+
+		if frappe.db.exists(
+			"Blocked Email Address",
+			{"user": self.user, "email": self.email, "name": ["!=", self.name]},
+		):
+			frappe.throw(
+				frappe._("The email address {0} is already blocked for user {1}.").format(
+					self.email, self.user
+				)
+			)
 
 
 def get_blocked_email_addresses(user: str) -> list[str]:
