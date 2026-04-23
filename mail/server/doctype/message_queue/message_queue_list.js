00478d1cf07bf0ae7b3c9261574e627fda274ce9
s-aga-r <sagarsharma.s312@gmail.com>
2026-04-23 12:23:54 +0530

    refactor: rmeove cluster from Message Queue

--- mail/server/doctype/message_queue/message_queue_list.js
+++ mail/server/doctype/message_queue/message_queue_list.js
@@ -12,69 +12,53 @@ function add_pause_resume_buttons(listview) {
 	if (!frappe.user_roles.includes('System Manager')) return
 	if (!listview.filters) return
 
-	const cluster_filter = listview.filters.find(
-		(f) => f[0] === 'Message Queue' && f[1] === 'cluster' && f[2] === '=',
-	)
-	const cluster = cluster_filter ? cluster_filter[3] : null
-
-	if (!cluster) return
-
 	frappe.call({
 		method: 'mail.server.doctype.message_queue.message_queue.get_queue_status',
-		args: {
-			cluster: cluster,
-		},
 		freeze: true,
 		freeze_message: __('Getting Queue Status...'),
 		callback: (r) => {
 			if (!r.exc) {
-				update_pause_resume_buttons(listview, cluster, r.message)
+				update_pause_resume_buttons(listview, r.message)
 			}
 		},
 	})
 }
 
-function update_pause_resume_buttons(listview, cluster, status) {
+function update_pause_resume_buttons(listview, status) {
 	if (status) {
 		listview.page.remove_inner_button('Resume')
 		listview.page.add_inner_button(__('Pause'), () => {
-			pause_queue(listview, cluster)
+			pause_queue(listview)
 		})
 	} else {
 		listview.page.remove_inner_button('Pause')
 		listview.page.add_inner_button(__('Resume'), () => {
-			resume_queue(listview, cluster)
+			resume_queue(listview)
 		})
 	}
 }
 
-function pause_queue(listview, cluster) {
+function pause_queue(listview) {
 	frappe.call({
 		method: 'mail.server.doctype.message_queue.message_queue.pause_queue',
-		args: {
-			cluster: cluster,
-		},
 		freeze: true,
 		freeze_message: __('Pausing Queue...'),
 		callback: (r) => {
 			if (!r.exc) {
-				update_pause_resume_buttons(listview, cluster, false)
+				update_pause_resume_buttons(listview, false)
 			}
 		},
 	})
 }
 
-function resume_queue(listview, cluster) {
+function resume_queue(listview) {
 	frappe.call({
 		method: 'mail.server.doctype.message_queue.message_queue.resume_queue',
-		args: {
-			cluster: cluster,
-		},
 		freeze: true,
 		freeze_message: __('Resuming Queue...'),
 		callback: (r) => {
 			if (!r.exc) {
-				update_pause_resume_buttons(listview, cluster, true)
+				update_pause_resume_buttons(listview, true)
 			}
 		},
 	})
