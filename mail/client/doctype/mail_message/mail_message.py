39b13d0327c594d9b0b8eab243c353388b0b79f9
s-aga-r <sagarsharma.s312@gmail.com>
2026-04-24 13:46:24 +0530

    fix: convert cid to url for attachments with cid

--- mail/client/doctype/mail_message/mail_message.py
+++ mail/client/doctype/mail_message/mail_message.py
@@ -1064,7 +1064,7 @@ def format_message(user: str, mailbox_map: dict, message: dict) -> dict:
 				params += f"&filename={quote(filename)}"
 			attachment["url"] = f"/api/method/mail.api.mail.get_attachment?{params}"
 
-			if attachment["disposition"] == "inline" and attachment["cid"] and formatted_message["html_body"]:
+			if attachment["cid"] and formatted_message["html_body"]:
 				formatted_message["html_body"] = convert_img_src_from_cid_to_url(
 					formatted_message["html_body"],
 					attachment["cid"],
