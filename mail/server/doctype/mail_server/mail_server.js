b1f2cbefb4155ee601561bad7bd61d7ed006566e
s-aga-r <sagarsharma.s312@gmail.com>
2026-05-05 16:39:03 +0530

    refactor: Mail Server

--- mail/server/doctype/mail_server/mail_server.js
+++ mail/server/doctype/mail_server/mail_server.js
@@ -24,14 +24,6 @@ frappe.ui.form.on('Mail Server', {
 		if (!frappe.user_roles.includes('System Manager')) return
 
 		frm.add_custom_button(
-			__('Generate Config'),
-			() => {
-				frm.trigger('generate_config')
-			},
-			__('Actions'),
-		)
-
-		frm.add_custom_button(
 			__('Verify SSH Connection'),
 			() => {
 				frm.trigger('verify_ssh_connection')
@@ -66,20 +58,6 @@ frappe.ui.form.on('Mail Server', {
 				__('Actions'),
 			)
 		}
-	},
-
-	generate_config(frm) {
-		frappe.call({
-			doc: frm.doc,
-			method: 'generate_config',
-			freeze: true,
-			freeze_message: __('Generating Config...'),
-			callback: (r) => {
-				if (!r.exc) {
-					frm.refresh()
-				}
-			},
-		})
 	},
 
 	verify_ssh_connection(frm) {
@@ -131,24 +109,5 @@ frappe.ui.form.on('Mail Server', {
 				}
 			},
 		})
-	},
-})
-
-frappe.ui.form.on('Mail Server ACME Provider', {
-	acme_providers_add(frm, cdt, cdn) {
-		const row = locals[cdt][cdn]
-
-		if (frm.doc.hostname) {
-			row.directory_id = frm.doc.hostname.replaceAll('.', '-')
-			row.domains = frm.doc.hostname
-			row.contact = 'postmaster@' + frm.doc.hostname
-		}
-		if (frm.doc.acme_providers.length < 2) {
-			row.default = 1
-		}
-		row.challenge = 'TLS-ALPN-01'
-		row.directory = 'https://acme-v02.api.letsencrypt.org/directory'
-		row.renew_before = 30
-		refresh_field('acme_providers')
 	},
 })
--- mail/server/doctype/mail_server/mail_server.py
+++ mail/server/doctype/mail_server/mail_server.py
@@ -13,11 +13,7 @@ import paramiko
 from frappe import _
 from frappe.model.document import Document
 
-from mail.server.doctype.server_config.server_config import create_server_config
 from mail.utils.dns import get_dns_record
-
-if TYPE_CHECKING:
-	from mail.server.doctype.server_config.server_config import ServerConfig
 
 
 class MailServer(Document):
@@ -34,14 +30,6 @@ class MailServer(Document):
 	def validate(self) -> None:
 		self.validate_hostname()
 		self.validate_cluster()
-		self.validate_base_url()
-		self.validate_cluster_node_id()
-		self.validate_acme_providers()
-		self.validate_tls_certificates()
-		self.validate_listeners()
-
-	def after_insert(self) -> None:
-		self.generate_config()
 
 	def on_trash(self) -> None:
 		if frappe.session.user != "Administrator":
@@ -61,101 +49,6 @@ class MailServer(Document):
 
 		if not frappe.db.get_value("Mail Cluster", self.cluster, "enabled"):
 			frappe.throw(_("Mail Cluster {0} is disabled.").format(frappe.bold(self.cluster)))
-
-	def validate_base_url(self) -> None:
-		"""Validates the base URL of the server."""
-
-		if not self.base_url:
-			self.base_url = f"https://{self.hostname}/"
-
-	def validate_cluster_node_id(self) -> None:
-		"""Validates the cluster node ID."""
-
-		if frappe.db.exists(
-			"Mail Server",
-			{
-				"cluster": self.cluster,
-				"cluster_node_id": self.cluster_node_id,
-				"name": ["!=", self.name],
-			},
-		):
-			frappe.throw(
-				_("Node ID {0} already assigned to another Mail Server.").format(
-					frappe.bold(self.cluster_node_id)
-				)
-			)
-
-	def validate_acme_providers(self) -> None:
-		"""Validates the ACME Providers."""
-
-		default_count = 0
-		directory_ids = []
-		for acme in self.acme_providers:
-			if acme.default:
-				default_count += 1
-
-			if acme.directory_id in directory_ids:
-				frappe.throw(
-					_("Row #{0}: Directory ID {1} is duplicated.").format(
-						acme.idx, frappe.bold(acme.directory_id)
-					)
-				)
-			directory_ids.append(acme.directory_id)
-
-		if default_count > 1:
-			frappe.throw(_("Only one ACME Provider can be default."))
-
-	def validate_tls_certificates(self) -> None:
-		"""Validates the TLS Certificates."""
-
-		default_count = 0
-		certificate_ids = []
-		for tls in self.tls_certificates:
-			if tls.default:
-				default_count += 1
-
-			if tls.certificate_id in certificate_ids:
-				frappe.throw(
-					_("Row #{0}: Certificate ID {1} is duplicated.").format(
-						tls.idx, frappe.bold(tls.certificate_id)
-					)
-				)
-			certificate_ids.append(tls.certificate_id)
-
-			if not tls.cert and not tls.cert_path:
-				frappe.throw(_("Row #{0}: Certificate or Certificate Path is required.").format(tls.idx))
-			if not tls.private_key and not tls.private_key_path:
-				frappe.throw(_("Row #{0}: Private Key or Private Key Path is required.").format(tls.idx))
-
-		if default_count > 1:
-			frappe.throw(_("Only one TLS Certificate can be default."))
-
-	def validate_listeners(self) -> None:
-		"""Validates the listeners."""
-
-		listener_ids = []
-		for listener in self.listeners:
-			if listener.listener_id in listener_ids:
-				frappe.throw(
-					_("Row #{0}: Listener ID {1} is duplicated.").format(
-						listener.idx, frappe.bold(listener.listener_id)
-					)
-				)
-
-			listener_ids.append(listener.listener_id)
-
-	@frappe.whitelist()
-	def generate_config(self) -> None:
-		"""Generates the Server Config."""
-
-		frappe.only_for("System Manager")
-		self._generate_config()
-		frappe.msgprint(_("Server Config created."), indicator="green", alert=True)
-
-	def _generate_config(self) -> "ServerConfig":
-		"""Generates the Server Config."""
-
-		return create_server_config(self.name)
 
 	@frappe.whitelist()
 	def verify_ssh_connection(self) -> None:
