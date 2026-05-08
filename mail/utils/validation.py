a4b841562dbcc91106697442225fa48c5afd8cef
s-aga-r <sagarsharma.s312@gmail.com>
2026-05-08 17:10:35 +0530

    fix: circular import

--- mail/utils/validation.py
+++ mail/utils/validation.py
@@ -1,4 +1,3 @@
-import fnmatch
 import ipaddress
 import os
 import re
@@ -11,14 +10,6 @@ from frappe.utils import cint
 from frappe.utils.caching import request_cache
 
 from mail.utils import get_mail_config
-from mail.utils.user import (
-	get_local_groups,
-	get_local_mailing_lists,
-	is_administrator,
-	is_local_user,
-	is_mail_admin,
-	is_system_manager,
-)
 
 
 def is_valid_host(host: str) -> bool:
@@ -335,6 +326,8 @@ def validate_nested_maildir_tree(base_dir: str, raise_exception: bool = False) -
 def has_permission_for_user(user: str, raise_exception: bool = True) -> bool:
 	"""Checks if the current user has permission to access the given user."""
 
+	from mail.utils.user import is_administrator
+
 	current_user = frappe.session.user
 	has_permission = user == current_user or is_administrator(current_user)
 
@@ -366,12 +359,16 @@ def validate_mail_config() -> None:
 def ensure_local_user(user: str) -> None:
 	"""Ensures that the user is a managed user."""
 
+	from mail.utils.user import is_local_user
+
 	if not is_local_user(user):
 		frappe.throw(_("User {0} is not a local user.").format(frappe.bold(user)))
 
 
 def ensure_access_to_backend() -> None:
 	"""Ensures that the current user has access to the mail backend."""
+
+	from mail.utils.user import is_mail_admin, is_system_manager
 
 	user = frappe.session.user
 	if not is_mail_admin(user) and not is_system_manager(user):
