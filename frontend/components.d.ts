78d6eea8900bf38444d946f220a3a39c323619b2
Akash Tom <me@akashtom.com>
2026-05-13 16:35:21 +0530

    feat: new maillistitem layour with more actions

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -70,6 +70,7 @@ declare module 'vue' {
     MailDetails: typeof import('./src/components/MailDetails.vue')['default']
     MailDetailsPopover: typeof import('./src/components/MailDetailsPopover.vue')['default']
     MailListItem: typeof import('./src/components/MailListItem.vue')['default']
+    MailListItemActions: typeof import('./src/components/MailListItemActions.vue')['default']
     MailLogo: typeof import('./src/components/Icons/MailLogo.vue')['default']
     MailThread: typeof import('./src/components/MailThread.vue')['default']
     MailThreadPlaceholder: typeof import('./src/components/MailThreadPlaceholder.vue')['default']
---
+++ frontend/src/components/MailListItemActions.vue
@@ -0,0 +1,75 @@
+<template>
+	<template v-if="isHovered">
+		<Tooltip v-for="action in actions" :key="action.label" :text="action.label">
+			<button
+				class="text-ink-gray-4 hover:!text-ink-gray-8"
+				@click.stop.prevent="action.onClick"
+			>
+				<component :is="action.icon" class="stroke-1.5 h-4 w-4" />
+			</button>
+		</Tooltip>
+	</template>
+	<Tooltip :text="__('Star Thread')">
+		<button
+			class="text-ink-gray-4 hover:!text-ink-gray-8"
+			@click.stop.prevent="console.log('star')"
+		>
+			<Star
+				class="stroke-1.5 h-4 w-4"
+				:class="{
+					'fill-ink-amber-2 text-ink-amber-2 stroke-ink-amber-2': mail.flagged,
+				}"
+			/>
+		</button>
+	</Tooltip>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { Archive, Mail, MailOpen, Star, Trash2 } from 'lucide-vue-next'
+import { Tooltip } from 'frappe-ui'
+
+import { userStore } from '@/stores/user'
+
+import type { Thread } from '@/types'
+
+const { isHovered, mail } = defineProps<{ isHovered: boolean; mail: Thread }>()
+const emit = defineEmits(['setSeen', 'trashThread', 'deleteThread'])
+
+const { mailboxIds } = userStore()
+
+const actions = computed(() =>
+	[
+		{
+			label: __('Mark as Unread'),
+			onClick: () => emit('setSeen', false),
+			icon: Mail,
+			condition: mail.seen,
+		},
+		{
+			label: __('Mark as Read'),
+			onClick: () => emit('setSeen', true),
+			icon: MailOpen,
+			condition: !mail.seen,
+		},
+		{
+			label: __('Archive Thread'),
+			onClick: () => emit('trashThread'),
+			icon: Archive,
+			condition: !mail.mailboxes.some((m) => m.mailbox_id === mailboxIds.trash),
+		},
+		{
+			label: __('Move to Trash'),
+			onClick: () => emit('trashThread'),
+			icon: Trash2,
+			condition: !mail.mailboxes.some((m) => m.mailbox_id === mailboxIds.trash),
+		},
+		{
+			label: __('Delete Thread'),
+			onClick: () => emit('deleteThread'),
+			icon: Trash2,
+			condition: mail.mailboxes.some((m) => m.mailbox_id === mailboxIds.trash),
+		},
+	].filter((action) => action.condition),
+)
+</script>
