3d7af424f63836588444d76cdd955e3856674e36
Akash Tom <me@akashtom.com>
2026-05-16 10:36:41 +0530

    fix(ThreadDivider): bigger button on mobile

--- frontend/src/components/ThreadDivider.vue
+++ frontend/src/components/ThreadDivider.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="text-ink-gray-5 flex items-center">
 		<span class="flex-1 border-t" />
-		<span class="rounded-full border px-2 py-1 text-sm"> {{ message }} </span>
+		<span class="rounded-full border px-3.5 py-2 text-sm sm:px-2 sm:py-1">
+			{{ message }}
+		</span>
 		<span class="flex-1 border-t" />
 	</div>
 </template>
