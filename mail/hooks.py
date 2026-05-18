3d2baed5a399da032957b807e099702d11fe0b70
s-aga-r <sagarsharma.s312@gmail.com>
2026-05-18 09:51:19 +0530

    chore: remove `auth.py`

--- mail/hooks.py
+++ mail/hooks.py
@@ -340,7 +340,7 @@ ignore_links_on_delete = [
 # Authentication and authorization
 # --------------------------------
 
-auth_hooks = ["mail.auth.validate"]
+# auth_hooks = ["mail.auth.validate"]
 
 # Automatically update python controller files with type annotations for this app.
 # export_python_type_annotations = True
